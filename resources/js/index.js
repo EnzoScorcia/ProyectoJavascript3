a35d791fa3666a1c91d34347ce3cabde6c8a5645
Enzo Scorciapino <enzoscorciapino05@gmail.com>
2024-10-14 15:46:50 -0300

    SalaDeJuegosV1

---
+++ resources/js/index.js
@@ -0,0 +1,11 @@
+function redireccionPPT(){
+    location.href = "../pages/PPT.html"
+}
+
+function redireccionJDM(){
+    location.href = "../pages/memory.html"
+}
+
+function redireccionTTT(){
+    location.href = "../pages/TTT.html"
+}
